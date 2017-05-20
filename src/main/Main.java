45bc6457acd29c3a71ed627295b62a8c1c8ee229
Macielos <maciek.ganko@gmail.com>
2017-05-20 14:25:37 +0200

    wsadowe odpalanie dla wielu plików i wielu unitCountów

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,18 +5,20 @@ import datasource.GraphFileLoader;
 import flowpathfinding.*;
 import results.ResultSaver;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Maciek on 14.05.2017.
  */
 public class Main {
 
-    public static final String INPUT_DIR = "graphs/";
-    public static final String OUTPUT_DIR = "results/";
+    public static final String OUTPUT_DIR = "results"+File.separator;
 
     private static GraphFileLoader graphFileLoader = new GraphFileLoader();
     private static IFlowPathFinder SPEflowPathFinder = new SPEModifiedFlowPathFinder();
@@ -29,19 +31,41 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         /* wczytanie grafu z pliku */
-        if(args.length != 3) {
-            System.out.println("Args: algorithm inputFile unitCount");
+        if(args.length < 3) {
+            System.out.println("Args: algorithm inputDir unitCount1 unitCount2...");
             return;
         }
         String algorithm = args[0];
-        String inputFile = args[1];
-        int unitCount = Integer.parseInt(args[2]);
+        String inputDir = args[1];
+        List<Integer> unitCounts = new ArrayList<>();
+        for(int i=2; i<args.length; ++i) {
+            unitCounts.add(Integer.parseInt(args[i]));
+        }
+        File dir = new File(inputDir);
+        if(!dir.exists() || !dir.isDirectory()) {
+            System.out.println("Invalid input dir");
+            return;
+        }
 
-        FlowPathParameters parameters = graphFileLoader.loadGraph(INPUT_DIR+inputFile, unitCount, algorithm);
-        System.out.println("Finding flow paths...");
+        System.out.println("Input dir "+dir.getPath());
+        for(File inputFile: dir.listFiles()) {
+            for(Integer unitCount: unitCounts) {
+                findFlowPaths(inputDir, simpleFileName(inputFile), algorithm, unitCount);
+            }
+        }
+    }
+
+    private static String simpleFileName(File inputFile) {
+        String path = inputFile.getPath();
+        int lastSlash = path.lastIndexOf(File.separator);
+        return lastSlash == -1 ? path : path.substring(lastSlash);
+    }
+
+    private static void findFlowPaths(String inputDir, String inputFile, String algorithm, Integer unitCount) throws IOException {
+        System.out.println("Finding flow paths for inputFile: "+inputFile+", algorithm: "+algorithm+", unitCount: "+unitCount);
+        FlowPathParameters parameters = graphFileLoader.loadGraph(inputDir+File.separator+inputFile, unitCount, algorithm);
 
         FlowPathResult flowPathResult = null;
-
         switch(algorithm) {
             case "SPE":
                 flowPathResult = SPEflowPathFinder.findFlowPaths(parameters);
